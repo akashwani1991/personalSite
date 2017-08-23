2976ccb546dbdf14036eae3329265a898a1d9f69
Akash Wani <apw3928@rit.edu>
2017-08-23 13:01:00 -0400

    Update email.php

--- pages/email.php
+++ pages/email.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_POST['submit']))
+if(!isset($_POST['connectMessage']))
 {
 	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
@@ -30,11 +30,16 @@ $email_body = "You have received a new message from the user $name.\n".
 $to = "apw3928@rit.edu";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: pages/home.html');
 
+// Send email
+if(mail($to,$email_subject,$email_body,$headers)) {
+        $status = 'ok';
+} else {
+        $status = 'err';
+}
+
+// Output status
+echo $status;die;
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
