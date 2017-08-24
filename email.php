6dff412b111947a029a4a40c48805305453b9b7c
Akash Wani <apw3928@rit.edu>
2017-08-24 00:25:36 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,6 +1,4 @@
 <?php
-require_once 'PHPMailerAutoload.php';
-
 if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
 
     //check if any of the inputs are empty
