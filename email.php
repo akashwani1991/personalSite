eb24aadfc4bf2118cabf0550f15123169e3c6b64
Akash Wani <apw3928@rit.edu>
2017-08-24 23:16:12 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,40 +1,26 @@
 <?php
-require_once 'phpmailer/PHPMailerAutoload.php';
+$response = array( 'success' => false );
+    $formD = file_get_contents( 'php://formData' );
+    $data = json_decode( $formD );
+    if ( $data->submit ) {
+        $name = $data->name;
+        $email = $data->email;
+        $subject = $data->subject;
+        $message = $data->message;
 
-if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
+        if ( $name != '' && $email != '' && $subject != '' && $message != '' ) {
+            $mailTo = 'apw3928@rit.edu';
+            $body  = 'From: ' . $name . "\n";
+            $body .= 'Email: ' . $email . "\n";
+            $body .= "Message:\n" . $message . "\n\n";
 
-    //check if any of the inputs are empty
-    if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['subject']) || empty($_POST['message'])) {
-        $data = array('success' => false, 'message' => 'Please fill out the form completely.');
-        echo json_encode($data);
-        exit;
+            $success = mail( $mailTo, $subject, $body );
+
+            if ( $success ) {
+                $response[ 'success' ] = true;
+            }
+        }
     }
 
-    //create an instance of PHPMailer
-    $mail = new PHPMailer();
-
-    $mail->From = $_POST['email'];
-    $mail->FromName = $_POST['name'];
-    $mail->AddAddress('apw3928@rit.edu'); //recipient 
-    $mail->Subject = $_POST['subject'];
-    $mail->Body = "Name: " . $_POST['name'] . "\r\n\r\nMessage: " . stripslashes($_POST['message']);
-
-    if (isset($_POST['ref'])) {
-        $mail->Body .= "\r\n\r\nRef: " . $_POST['ref'];
-    }
-
-    if(!$mail->send()) {
-        $data = array('success' => false, 'message' => 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
-        echo json_encode($data);
-        exit;
-    }
-
-    $data = array('success' => true, 'message' => 'Thanks! We have received your message.');
-    echo json_encode($data);
-
-} else {
-
-    $data = array('success' => false, 'message' => 'Please fill out the form completely.');
-    echo json_encode($data);
-
-}
+    echo json_encode( $response );
+?>
